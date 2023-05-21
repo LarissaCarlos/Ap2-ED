609c1c224b3113cfde7b3c4d7c1bd2a70f60d64f
LarissaCarlos <mpkavolt.l@rede.ulbra.br>
2023-05-20 23:15:53 -0300

    Ap2-Parcial

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Node {
+public class Node {
+    String value;
+    int data;
+        Node prev;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+
 }
--- src/Program.java
+++ src/Program.java
@@ -1,134 +1,10 @@
 import java.util.Scanner;
 
-public class Main {
+public class Program {
 
-    class Node {
-        int data;
-        Node prev;
-        Node next;
-
-        public Node(int data) {
-            this.data = data;
-            this.prev = null;
-            this.next = null;
-        }
-    }
-
-    class DoublyLinkedList {
-        Node head;
-        Node tail;
-
-        public DoublyLinkedList() {
-            this.head = null;
-            this.tail = null;
-        }
-
-        public void insertAtBeginning(int data) {
-            Node newNode = new Node(data);
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                newNode.next = head;
-                head.prev = newNode;
-                head = newNode;
-            }
-        }
-
-        public void insertAtEnd(int data) {
-            Node newNode = new Node(data);
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                newNode.prev = tail;
-                tail = newNode;
-            }
-        }
-
-        public void removeFromBeginning() {
-            if (head == null) {
-                System.out.println("A lista está vazia.");
-            } else {
-                if (head == tail) {
-                    head = null;
-                    tail = null;
-                } else {
-                    head = head.next;
-                    head.prev = null;
-                }
-            }
-        }
-
-        public void removeFromEnd() {
-            if (tail == null) {
-                System.out.println("A lista está vazia.");
-            } else {
-                if (head == tail) {
-                    head = null;
-                    tail = null;
-                } else {
-                    tail = tail.prev;
-                    tail.next = null;
-                }
-            }
-        }
-
-        public int getValueAtPosition(int position) {
-            if (position < 0 || head == null) {
-                System.out.println("Posição inválida ou a lista está vazia.");
-                return -1;
-            }
-
-            Node currentNode = head;
-            int currentPosition = 0;
-
-            while (currentNode != null && currentPosition < position) {
-                currentNode = currentNode.next;
-                currentPosition++;
-            }
-
-            if (currentNode == null) {
-                System.out.println("Posição inválida.");
-                return -1;
-            }
-
-            return currentNode.data;
-        }
-
-        public void printForward() {
-            if (head == null) {
-                System.out.println("A lista está vazia.");
-            } else {
-                Node currentNode = head;
-                while (currentNode != null) {
-                    System.out.print(currentNode.data + " ");
-                    currentNode = currentNode.next;
-                }
-                System.out.println();
-            }
-        }
-
-        public void printReverse() {
-            if (tail == null) {
-                System.out.println("A lista está vazia.");
-            } else {
-                Node currentNode = tail;
-                while (currentNode != null) {
-                    System.out.print(currentNode.data + " ");
-                    currentNode = currentNode.prev;
-                }
-                System.out.println();
-            }
-        }
-    }
-
-    public class Main {
-        public void main(String[] args) {
-            DoublyLinkedList list = new DoublyLinkedList();
+        public static void main(String[] args) {
+            List list = new List();
             Scanner scanner = new Scanner(System.in);
-
             int choice = 0;
             while (choice != 7) {
                 System.out.println("Escolha uma opção:");
@@ -187,6 +63,5 @@ public class Main {
 
             scanner.close();
         }
-    }
 
 }
